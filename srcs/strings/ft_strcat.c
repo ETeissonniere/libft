bf34c6b909bae1bf9d8f9bf187f6ddbb60e1a9c9
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-01-12 18:07:09 +0100

    Cleanup strcat

--- srcs/strings/ft_strcat.c
+++ srcs/strings/ft_strcat.c
@@ -4,10 +4,7 @@ static void __strcat(char *s1, const char *s2) {
     while (*s1)
         s1++;
 
-    while (*s2)
-        *(s1++) = *(s2++);
-
-    *s1 = '\0';
+    ft_strcpy(s1, s2);
 }
 
 char *ft_strcat(char *s1, const char *s2) {
