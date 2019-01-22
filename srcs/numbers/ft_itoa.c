ac8aa41a4293c74ac095d0e5f8e0d9e2ca9b59f1
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-01-22 11:20:55 +0100

    tune itoa

--- srcs/numbers/ft_itoa.c
+++ srcs/numbers/ft_itoa.c
@@ -1,32 +1,39 @@
 #include "libft.h"
 
+static void __compute_size(int n, int *size) {
+    while (n /= 10)
+        (*size)++;
+}
+
 char *ft_itoa(int n) {
     int sign;
     int size;
-    int pos;
     char *as_str;
+
+    if (n == -2147483648)
+        return ft_strdup("-2147483648");
 
     sign = (n < 0);
     if (sign)
         n = -n;
 
-    size = 0;
-    while (ft_power(10, size) < n)
-        size++;
+    size = 2;
+    __compute_size(n, &size);
+    size += sign;
 
-    as_str = ft_strnew(size + 1);
+    as_str = ft_strnew(size);
     if (as_str == NULL)
         return NULL;
 
-    pos = 1;
-    as_str[0] = (n % 10) + '0';
-    while ((n /= 10) > 0)
-        as_str[pos++] = (n % 10) + '0';
+    as_str[--size] = '\0';
+
+    while (size--) {
+        as_str[size] = (n % 10) + '0';
+        n /= 10;
+    }
 
     if (sign)
-        as_str[pos++] = '-';
+        as_str[0] = '-';
 
-    as_str[size + 1] = '\0';
-
-    return ft_strrev(as_str);
+    return as_str;
 }
